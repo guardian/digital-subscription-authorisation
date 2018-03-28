903636f55ed22b7f8a0e119af3665887a85524f5
pvighi <patricio.vighi@guardian.co.uk>
2018-03-28 15:03:24 +0100

    fix to js script

--- build-js.sh
+++ build-js.sh
@@ -2,7 +2,7 @@
 
 cd js
 #Installing node 6.10
-#NODE_VERSION="6.10"
+NODE_VERSION="6.10"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm install ${NODE_VERSION}
@@ -18,6 +18,7 @@ if [ ! -d "$YARN_LOCATION" ]; then
 	wget -qO- https://github.com/yarnpkg/yarn/releases/download/v${YARN_VERSION}/yarn-v${YARN_VERSION}.tar.gz | tar zvx
 	cd ../..
 fi
+PATH="$PATH:${YARN_LOCATION}/dist/bin/"
 PATH="$PATH:${YARN_LOCATION}/dist/bin/"
 
 # Installing packages via yarn
